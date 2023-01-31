0c1e295c31bda08e416e8e26f0442cfd6034a568
Sebastian Lesch <sebastian.lesch@gmail.com>
2023-01-31 13:20:54 -0500

    Adding amplify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    json,
+    resolve({
+      browser: true,
+      preferBuiltins: false,
+    }),
+  ],
+  define: {
+    global: 'window',
+  },
+  resolve: {
+    browser: true,
+    preferBuiltins: false,
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
 })
