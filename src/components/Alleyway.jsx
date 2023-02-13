ada7675a33897a7e8ca1cf1a8c64037b173d45c6
Sebastian Lesch <sebastian.lesch@gmail.com>
2023-02-13 18:36:02 -0500

    Rework v3 first attempt at mobile fixing

--- src/components/Alleyway.jsx
+++ src/components/Alleyway.jsx
@@ -169,7 +169,12 @@ export default function Alleyway({ page }) {
 
   return (
     <>
-      <div className={"mobile copy copy-display " + transitionState}>{copy}</div>
+      <div className={"mobile copy copy-display " + transitionState}>
+        {copy}
+        {link ? (<a className='project-link' href={link} target="_blank">
+          <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21 3L15 3M21 3L12 12M21 3V9" stroke="white" strokeLinecap="round" strokeLinejoin="round" /> <path d="M21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H11" stroke="white" strokeLinecap="round" /> </svg>
+        </a>) : ''}
+      </div>
 
       <div className="nightsky"></div>
       <div className={"alleyway " + transitionState} data-numstores={num_stores}>
